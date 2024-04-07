97395c034bb071d4556e13545e69532f7a99f45d
Richerlv <richerlv@foxmail.com>
2024-04-07 14:57:34 +0800

    '处理跨域请求

--- src/main/java/com/example/route/controller/HelloController.java
+++ src/main/java/com/example/route/controller/HelloController.java
@@ -3,8 +3,10 @@ package com.example.route.controller;
 import com.example.route.mapper.TestMapper;
 import com.example.route.pojo.Test;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +19,8 @@ import java.util.List;
  * @description:
  */
 
+@Slf4j
+@CrossOrigin
 @Controller
 public class HelloController {
 
@@ -26,6 +30,7 @@ public class HelloController {
     @ResponseBody
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("SOMEONE SAY HELLO!");
         return "hello!";
     }
 
